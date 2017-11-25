87113fc87b1cb9fdf139bf384e4c0b31c4cc7ba4
13091987Y <atticot.yohann@gmail.com>
2017-11-25 09:37:39 +0100

    Authentification with Google

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send({hello: 'Standard'});
-});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken)=>{ console.log(accessToken); }));
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }) 
+);
+
+app.get(
+    '/auth/google/callback',
+    (req, res)=>{ 
+        passport.authenticate('google');
+        res.send({'hi': req.body}) }
+);
 
 // env Production for Heroku 
 const PORT = process.env.PORT || 5003;
