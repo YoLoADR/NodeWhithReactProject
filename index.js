292c077efa4f7c6f0df3e7b28b7e9fa6d85c267d
13091987Y <atticot.yohann@gmail.com>
2017-11-26 09:49:02 +0100

    Video 33 34 - Connecting Mongoose to Mongo

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 //Trick - we just need a function, it's not necessary to have more variable 
 require('./services/passport');
+
+//We should pass the adress of online database in there
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 //Trick - it's function execute automatically, (app) it's params, you can take a look in his file for understand
